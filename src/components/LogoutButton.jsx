3b1fd5acab98bcfb783c214fa70b2a85537fc876
Fnrzz <faridnurrganteng123@gmail.com>
2024-12-01 15:28:33 +0700

    [PUSH] fixing logoutbutton

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -1,7 +1,10 @@
+import { useNavigate } from "react-router-dom";
 const LogoutButton = () => {
+  const navigate = useNavigate();
   const handlerLogout = () => {
     localStorage.removeItem("access_token");
     window.dispatchEvent(new Event("storage"));
+    navigate("/");
   };
   return (
     <button onClick={handlerLogout} className="nav-link">
